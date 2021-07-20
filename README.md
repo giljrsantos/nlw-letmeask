154474d94a28bae01ad7bd3aee653568b6e866f9
GILBERTO JUNIOR DOS SANTOS <giljrsantos@gmail.com>
2021-07-20 07:39:32 -0300

    Finalizando projto Nlw6

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # LetMeask
 
+O o intudo do projeto foi criar uma sala de perguntas.
+Usando a autenticação da sua conta no Google.
+
+O usário pode criar uma nova sala, ou logar em uma sala já existente.
+
+Podem fazer perguntas e da like nas outras perguntas existentes. 
+
+O administrador da sala, pode marcar a pergunta como respondida ou remove-la caso seja uma pergunta que não tenha relação com o nome da sala.
+
+
 ## Business Rules
     [ x ] Only authenticated users read data fro rooms and questions;
     [ x ] Only authenticated users can create new rooms
@@ -9,12 +19,41 @@
     [ x ] User can't update or delete questions highlight and answered data;
     [ x ] Only the room owner can close the room;
     [ x ] Don't allow duplicated likes from sama author;
-    [  ] Don't allow user to like own questions;
+    [ x ] Don't allow user to like own questions;
 
+# DEPENDÊNCIAS
+    "classnames": "^2.3.1",
+    "firebase": "^8.7.1",
+    "node-sass": "^5.0.0",
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2",
+    "react-router-dom": "^5.2.0",
+    "react-scripts": "4.0.3",
+    "typescript": "^4.1.2",
+    "web-vitals": "^1.0.1"
+
+# Dependências de Desenvolvimento
+    "@types/react-router-dom": "^5.1.8"  
+
+# 📱 ScreenShot
+### Tela de login ou ingressar em uma sala existente
 <p align="center">
   <img src="./src/assets/images/img-01.png" width="800" title="Autentique ou entre em uma sala existente!">
-  <img src="./src/assets/images/img-02.png" width="800" alt="Crie uma nova Sala">
-  <img src="./src/assets/images/img-03.png" width="800" alt="Crie uma nova Sala">
-  <img src="./src/assets/images/img-04.png" width="800" alt="Crie uma nova Sala">
-  <img src="./src/assets/images/img-05.png" width="800" alt="Crie uma nova Sala">
 </p>
+
+### Tela para criar uma sala
+<p align="center">
+  <img src="./src/assets/images/img-02.png" width="800" alt="Crie uma nova Sala">
+</p>
+
+#### Sala criada com perguntas e a pergunta com o like.
+<p align="center">
+  <img src="./src/assets/images/img-04.png" width="800" alt="Crie uma nova Sala">
+</p>
+
+#### Tela onde o Adminstrador consegui excluir, marcar como respondida e da check para que pergunta não seja mais respondida.
+<p align="center">
+  <img src="./src/assets/images/img-07.png" width="800" alt="Crie uma nova Sala">
+</p>
+
+<h1 align="center">💻 Desenvolvido Por: Gilberto Júnior</h1>
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,6 +1,10 @@
-import { useHistory, useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom';
+
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
+
 import { Button } from '../components/Button';
 import { Questions } from '../components/Question/index';
 import { RoomCode } from '../components/RoomCode';
@@ -40,6 +44,17 @@ export function AdminRoomm() {
         }
     }
 
+    async function handleCheckQuestionAsAnswred(questionId: string){
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true
+        });
+    }
+    async function handleHighlightQuestion(questionId: string){
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true
+        });
+    }
+
     return (
         <div id="page-room">
             <header>
@@ -66,7 +81,25 @@ export function AdminRoomm() {
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
+                                isAnswered={question.isAnswered}
+                                isHighlighted={question.isHighlighted}
                             >
+                                {!question.isAnswered && (
+                                <>
+                                    <button
+                                    type="button"
+                                        onClick={() => handleCheckQuestionAsAnswred(question.id)}
+                                    >
+                                        <img src={checkImg} alt="Marcar pergunta como respondida" />
+                                    </button>
+                                    <button
+                                        type="button"
+                                        onClick={() => handleHighlightQuestion(question.id)}
+                                    >
+                                        <img src={answerImg} alt="Dar destaque a pergunta" />
+                                    </button>
+                                </>
+                                )}
                                 <button
                                     type="button"
                                     onClick={() => handleDeleteQuestion(question.id)}
